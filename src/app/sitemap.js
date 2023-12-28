9f96933fef45155d6f8d3eb17e3db806dc021a07
Abhirup Kumar Bhowmick <abhirupkumar2003@gmail.com>
2023-12-28 23:53:47 +0530

    commit

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,8 +1,8 @@
 export default async function sitemap() {
-    const routearray = ['', '/contact', '/refund', '/privacy', '/terms', '/pricing']
+    const routearray = ['https://docwhisperer.me', 'https://docwhisperer.me/contact', 'https://docwhisperer.me/refund', 'https://docwhisperer.me/privacy', 'https://docwhisperer.me/terms', 'https://docwhisperer.me/pricing']
 
     const routes = routearray.map((prod) => ({
-        url: `${process.env.NEXT_PUBLIC_HOST}/${prod}`,
+        url: `${prod}`,
         lastModified: new Date().toISOString(),
     }));
 
